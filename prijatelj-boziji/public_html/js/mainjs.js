92ef893b1f6abe49f6488309efd8958a192631a0
scococ <stefan_petrovic_92@hotmail.com>
2017-04-27 15:54:15 +0200

    nav jquery

--- prijatelj-boziji/public_html/js/mainjs.js
+++ prijatelj-boziji/public_html/js/mainjs.js
@@ -1,19 +1,23 @@
 
 
-$(document).ready(function(){
-    
-   
-    $('.albumm').fancygallery();
-    
-    $(function(scrollFunction){
-    jQuery.scrollSpeed(100, 1800, 'easeOutCubic');
-    
+var windowWidth = $(window).width();
+if (windowWidth > 768) {
+    $('.dropdown').hover(function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    }, function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
     });
-    
-    
-    
-    
-    
-    
+}
+
+
+$(document).ready(function () {
+
+    $('.albumm').fancygallery();
+
+    $(function (scrollFunction) {
+        jQuery.scrollSpeed(100, 1800, 'easeOutCubic');
+
+    });
+
 });
 
