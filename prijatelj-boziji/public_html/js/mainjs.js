dcab940e276372061fb97353b3a1f5066fc6a621
ABC <ABC@ABC-PC>
2017-05-04 11:57:52 +0200

    navbar brand

--- prijatelj-boziji/public_html/js/mainjs.js
+++ prijatelj-boziji/public_html/js/mainjs.js
@@ -1,16 +1,21 @@
 
-
-var windowWidth = $(window).width();
-if (windowWidth > 768) {
-    $('.dropdown').hover(function () {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-    }, function () {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+ $(window).on('load resize', function() {
+        var x = $(window).width();
+        
+        if (x >= 768) {
+            $('.dropdown').hover(function() {
+                $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+            }, function() {
+                $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+            });     
+        }
     });
-}
 
 
 $(document).ready(function () {
+
+
+   
 
     $('.albumm').fancygallery();
 
