64c1c23c9112e854387fc89ffca25173a3f639eb
ABC <ABC@ABC-PC>
2017-05-09 11:19:00 +0200

    dropdown hover

--- prijatelj-boziji/public_html/js/mainjs.js
+++ prijatelj-boziji/public_html/js/mainjs.js
@@ -1,28 +1,39 @@
+//
+// $(window).on('load resize', function() {
+//        var x = $(window).width();
+//        
+//        if (x >= 768) {
+//            $('.dropdown').hover(function() {
+//                $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+//            }, function() {
+//                $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+//            });     
+//        }
+//    });
 
- $(window).on('load resize', function() {
-        var x = $(window).width();
-        
-        if (x >= 768) {
-            $('.dropdown').hover(function() {
-                $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-            }, function() {
-                $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-            });     
-        }
-    });
+// $(function () {
+//            $(".dropdown").hover(function () {
+//                $(this).addClass('open');
+//            },
+//                    function () {
+//                        $(this).removeClass('open');
+//                    }
+//            );
+//        });
+
+
 
 
 $(document).ready(function () {
+//   $('.navigation-control .dropdown').mouseenter(function(){
+//       $('.navigation-control .dropdown:hover .dropdown-menu').delay(500).css('display', 'block');
+//       $('.navigation-control .dropdown').mouseleave(function(){
+//           $('.navigation-control .dropdown-menu').fadeOut('fast');
+//       });
+//   });
 
 
-   
 
     $('.albumm').fancygallery();
-
-    $(function (scrollFunction) {
-        jQuery.scrollSpeed(100, 1800, 'easeOutCubic');
-
-    });
-
 });
 
